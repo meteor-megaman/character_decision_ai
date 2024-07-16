0dbd0dc5f64e8c85fe78481e5e48b77a2dfb0af7
DESKTOP-3K13D8B\81902 <tmatma.oor@gmail.com>
2024-07-16 23:59:41 +0900

    画像データをもとに推論する

--- predictor.py
+++ predictor.py
@@ -1,13 +1,33 @@
 from keras import models
 import matplotlib.pyplot as plt
 import numpy as np
-
+import base64
+from PIL import Image
+from io import BytesIO
+import os
 
 def main():
-    model = load_model()
-    test_data = load_test_data()
-    make_predictions(model, test_data)
+    image_array = import_image('out/x_data/image_0.png')
+    print(image_array.shape)
+    encoded_image = encode_image_to_base64(image_array)
+    predict(encoded_image)
 
+def predict(encoded_data):
+    model = load_model()
+    make_predictions(model, encoded_data)
+
+def import_image(file_path):
+    with Image.open(file_path) as image:
+        image_array = np.array(image)
+    return image_array
+
+def encode_image_to_base64(image_array):
+    image = Image.fromarray(image_array)
+    buffered = BytesIO()
+    image.save(buffered, format='PNG')
+    image_bytes = buffered.getvalue()
+    encoded_data = base64.b64encode(image_bytes)
+    return encoded_data
     
 def load_test_data():
     data = np.load('out/mnist_data.npz')
@@ -16,29 +36,31 @@ def load_test_data():
         'y_test': data['y_test']
     }
 
-def make_predictions(model, test_data):
-    correct_num = 0
-    invalid_num = 0
-    all_num = len(test_data['x_test'])
-    for x_test, y_test in zip(test_data['x_test'][-100:-1], test_data['y_test'][-100:-1]):
-        x_test_reshaped = x_test.reshape(1, 28, 28)
-        predictions = model.predict(x_test_reshaped)
-        # predictionの中で最も確率の高いもののindexを取得
-        predict_num = np.argmax(predictions[0])
-        result = predict_num == y_test
-        if result:
-            correct_num += 1
-        else:
-            invalid_num += 1
-            plt.imshow(x_test, cmap='gray')
-            plt.savefig(f'out/invalid/invalid_{y_test}.png')
-        print('-----------------')
-    print(f'correct: {correct_num}, invalid: {invalid_num}, all: {all_num}')
+def make_predictions(model, base64_image):
+    def _decode_base64_to_string(base64_image):
+        image_data  = base64.b64decode(base64_image)
+        image = Image.open(BytesIO(image_data))
+        return np.array(image)
+    
+    x_data = _decode_base64_to_string(base64_image)
+    x_data_reshaped = x_data.reshape(1, 28, 28)
+    prediction = model.predict(x_data_reshaped)
+    predict_num = np.argmax(prediction[0])
+    print('==============================')
+    print(f'predict_num: {predict_num}')
+    print('==============================')
 
 
 def load_model():
     model = models.load_model('out/mnist_model.h5')
     return model
+
+def save_images():
+    test_data = load_test_data()
+    x_data = test_data['x_test']
+    for i in range(10):
+        image = Image.fromarray((x_data[i] * 255).astype('uint8'), 'L')
+        image.save(os.path.join('out/x_data', f'image_{i}.png'))
 
 if __name__ == '__main__':
     main()
