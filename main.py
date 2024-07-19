ef879bd46fc8e69fc976850c29d4a95ae05e71ef
DESKTOP-3K13D8B\81902 <tmatma.oor@gmail.com>
2024-07-20 08:58:19 +0900

    predictを利用シーンに合わせて修正

---
+++ main.py
@@ -0,0 +1,30 @@
+from predictor import predict
+from PIL import Image
+from io import BytesIO
+import base64
+import numpy as np
+
+
+def main():
+    model_type = 'mnist_model'
+    image_path = 'out/x_data/image_0.png'
+
+    image_base64 = transform_np_to_b64(image_path)
+    
+
+    result = predict(model_type, image_base64)
+    print(result)
+
+
+def transform_np_to_b64(image_path: str) -> str:
+    with Image.open(image_path) as image:
+        image_array = np.array(image)
+    image = Image.fromarray(image_array)
+    buffered = BytesIO()
+    image.save(buffered, format='PNG')
+    image_bytes = buffered.getvalue()
+    encoded_data = base64.b64encode(image_bytes)
+    return encoded_data
+
+if __name__ == '__main__':
+    main()
--- predictor.py
+++ predictor.py
@@ -1,66 +1,30 @@
 from keras import models
-import matplotlib.pyplot as plt
 import numpy as np
 import base64
 from PIL import Image
 from io import BytesIO
-import os
 
-def main():
-    image_array = import_image('out/x_data/image_0.png')
-    print(image_array.shape)
-    encoded_image = encode_image_to_base64(image_array)
-    predict(encoded_image)
 
-def predict(encoded_data):
-    model = load_model()
-    make_predictions(model, encoded_data)
-
-def import_image(file_path):
-    with Image.open(file_path) as image:
-        image_array = np.array(image)
-    return image_array
-
-def encode_image_to_base64(image_array):
-    image = Image.fromarray(image_array)
-    buffered = BytesIO()
-    image.save(buffered, format='PNG')
-    image_bytes = buffered.getvalue()
-    encoded_data = base64.b64encode(image_bytes)
-    return encoded_data
+def predict(model_type: str, image_b64: str) -> int:
+    if model_type not in ['mnist_model', 'model_B']:
+        raise ValueError('model_type should be either mnist_model or model_B')
     
-def load_test_data():
-    data = np.load('out/mnist_data.npz')
-    return {
-        'x_test': data['x_test'],
-        'y_test': data['y_test']
-    }
+    model_path = f'out/{model_type}.h5'
 
-def make_predictions(model, base64_image):
-    def _decode_base64_to_string(base64_image):
-        image_data  = base64.b64decode(base64_image)
-        image = Image.open(BytesIO(image_data))
-        return np.array(image)
-    
-    x_data = _decode_base64_to_string(base64_image)
-    x_data_reshaped = x_data.reshape(1, 28, 28)
-    prediction = model.predict(x_data_reshaped)
-    predict_num = np.argmax(prediction[0])
-    print('==============================')
-    print(f'predict_num: {predict_num}')
-    print('==============================')
+    model = load_model(model_path)
+    image_np = decode_base64_to_np(image_b64)
+    image_reshaped = image_np.reshape(1, 28, 28)
+    # 1つの画像のみ推論するため先頭要素のみ取得
+    prediction = model.predict(image_reshaped)[0]
+    # predictionは0~9となる確率が順に入っているため、最も数値が大きい要素のindexを取得
+    predict_num = np.argmax(prediction)
+    return predict_num
 
-
-def load_model():
-    model = models.load_model('out/mnist_model.h5')
+def load_model(model_path: str):
+    model = models.load_model(model_path)
     return model
 
-def save_images():
-    test_data = load_test_data()
-    x_data = test_data['x_test']
-    for i in range(10):
-        image = Image.fromarray((x_data[i] * 255).astype('uint8'), 'L')
-        image.save(os.path.join('out/x_data', f'image_{i}.png'))
-
-if __name__ == '__main__':
-    main()
+def decode_base64_to_np(base64_image: str) -> np.ndarray:
+    image_data  = base64.b64decode(base64_image)
+    image = Image.open(BytesIO(image_data))
+    return np.array(image)
